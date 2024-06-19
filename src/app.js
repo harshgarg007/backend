49650d3311ab544c4fb7a21c5af944cecd5584f4
Harsh Garg <123346497+harshgarg007@users.noreply.github.com>
2024-06-19 10:57:45 +0530

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+// These are middleware functions in Express.js, a web application framework for Node.js.
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended:true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser());
+
+
+export { app };
